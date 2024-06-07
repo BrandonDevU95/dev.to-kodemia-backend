cc529f87ef3f04c5d4a65c3311316b2c6230df27
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-07 07:42:22 -0600

    feat: Add getAllPosts controller to backend

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -26,4 +26,18 @@ const createPost = async (req, res) => {
 	}
 };
 
-module.exports = { createPost };
+const getAllPosts = async (req, res) => {
+	try {
+		const posts = await Post.find().sort({ created_at: -1 });
+
+		if (!posts) {
+			return res.status(404).json({ error: 'No posts found' });
+		}
+
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+};
+
+module.exports = { createPost, getAllPosts };
