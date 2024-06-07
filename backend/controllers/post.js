ae8b031ce01c92e7268c1a4f51e9dc2cb462424d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-07 12:22:06 -0600

    chore: Add deletePost controller to backend

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -91,4 +91,30 @@ const updatePost = async (req, res) => {
 	}
 };
 
-module.exports = { createPost, getAllPosts, getPostById, updatePost };
+const deletePost = async (req, res) => {
+	const { id } = req.params;
+
+	if (!id) {
+		return res.status(400).json({ error: 'Post ID is required' });
+	}
+
+	try {
+		const post = await Post.findByIdAndDelete({ _id: id });
+
+		if (!post) {
+			return res.status(404).json({ error: 'Post not found' });
+		}
+
+		res.status(200).json({ message: 'Post deleted successfully!' });
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+};
+
+module.exports = {
+	createPost,
+	getAllPosts,
+	getPostById,
+	updatePost,
+	deletePost,
+};
