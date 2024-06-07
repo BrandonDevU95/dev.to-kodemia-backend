40f06e54d369744f39790bec7c992abdeca8d5b8
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-07 12:22:10 -0600

    chore: Add deletePost API route to backend

--- backend/router/post.js
+++ backend/router/post.js
@@ -8,5 +8,6 @@ api.post('/posts', userAuth, PostController.createPost);
 api.get('/posts', PostController.getAllPosts);
 api.get('/posts/:id', PostController.getPostById);
 api.patch('/posts/:id', userAuth, PostController.updatePost);
+api.delete('/posts/:id', userAuth, PostController.deletePost);
 
 module.exports = api;
