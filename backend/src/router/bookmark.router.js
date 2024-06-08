3a620adfd1cb6100181a304f88fb24cc93260933
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-08 09:31:10 -0600

    chore: Update bookmark router to use async/await syntax for CRUD operations

--- backend/src/router/bookmark.router.js
+++ backend/src/router/bookmark.router.js
@@ -1,16 +1,98 @@
 const express = require('express');
-const BookmarkController = require('../usecases/bookmark.usecase');
+const bookmarkUsecase = require('../usecases/bookmark.usecase');
 const { userAuth } = require('../middlewares/auth.middleware');
 
 const api = express.Router();
 
-api.post('/bookmarks', userAuth, BookmarkController.createBookmark);
-api.get('/bookmarks', userAuth, BookmarkController.getAllBookmarks);
-api.get(
-	'/bookmarks/:post_id',
-	userAuth,
-	BookmarkController.getBookmarkIdByPost
-);
-api.delete('/bookmarks/:post_id', userAuth, BookmarkController.deleteBookmark);
+api.post('/bookmarks', userAuth, async (req, res) => {
+	try {
+		const { user_id } = req.user;
+		const { post_id } = req.body;
+
+		const savedBookmark = await bookmarkUsecase.createBookmark(
+			user_id,
+			post_id
+		);
+
+		res.status(201).json({
+			success: true,
+			message: 'Bookmark created successfully',
+			data: savedBookmark,
+		});
+	} catch (error) {
+		res.status(error.status || 500);
+
+		res.json({
+			succes: false,
+			error: error.message,
+		});
+	}
+});
+api.get('/bookmarks', userAuth, async (req, res) => {
+	try {
+		const { user_id } = req.user;
+
+		const bookmarks = await bookmarkUsecase.getAllBookmarks(user_id);
+
+		if (!bookmarks) {
+			throw createError(404, 'No bookmarks found');
+		}
+
+		res.status(200).json(bookmarks);
+	} catch (error) {
+		res.status(error.status || 500);
+
+		res.json({
+			succes: false,
+			error: error.message,
+		});
+	}
+});
+
+api.get('/bookmarks/:post_id', userAuth, async (req, res) => {
+	try {
+		const { user_id } = req.user;
+		const { post_id } = req.params;
+
+		const bookmark = await bookmarkUsecase.getBookmarkIdByPost(
+			user_id,
+			post_id
+		);
+
+		res.status(200).json(bookmark);
+	} catch (error) {
+		res.status(error.status || 500);
+
+		res.json({
+			succes: false,
+			error: error.message,
+		});
+	}
+});
+
+api.delete('/bookmarks/:post_id', userAuth, async (req, res) => {
+	try {
+		const { user_id } = req.user;
+		const { post_id } = req.params;
+
+		const bookmarkDeleted = await bookmarkUsecase.deleteBookmark(
+			user_id,
+			post_id
+		);
+
+		res.status(200).json({
+			deleted: true,
+			message: 'Bookmark deleted successfully!',
+			data: bookmarkDeleted,
+		});
+	} catch (error) {
+		res.status(error.status || 500);
+
+		res.json({
+			succes: false,
+			error: error.message,
+		});
+	}
+});
 
 module.exports = api;
--- backend/src/usecases/bookmark.usecase.js
+++ backend/src/usecases/bookmark.usecase.js
@@ -2,118 +2,68 @@ const Bookmark = require('../models/bookmark.model');
 const { validateBookmark } = require('../schemas/bookmark.schema');
 const createError = require('http-errors');
 
-const createBookmark = async (req, res) => {
-	const { user_id } = req.user;
-	const { post_id } = req.body;
-
-	const bookmarkData = {
+async function createBookmark(user_id, post_id) {
+	const bookmarkData = validateBookmark({
+		user: user_id,
 		post: post_id,
+	});
+
+	if (!bookmarkData.success) {
+		throw createError(400, JSON.parse(bookmarkData.error.message));
+	}
+
+	const bookmark = new Bookmark(bookmarkData.data);
+
+	const bookmarkExists = await Bookmark.findOne({
 		user: user_id,
-	};
-	const bookmarkFields = validateBookmark(bookmarkData);
+		post: post_id,
+	});
 
-	if (!bookmarkFields.success) {
-		throw createError(400, JSON.parse(bookmarkFields.error.message));
+	if (bookmarkExists) {
+		throw createError(400, 'Bookmark already exists');
 	}
 
-	const tempBookmark = {
-		...bookmarkFields.data,
+	const savedBookmark = await bookmark.save();
+	return savedBookmark;
+}
+
+async function getAllBookmarks(user_id) {
+	const bookmarks = await Bookmark.find({ user: user_id }).sort({
+		created_at: -1,
+	});
+
+	if (!bookmarks) {
+		throw createError(404, 'No bookmarks found');
+	}
+
+	return bookmarks;
+}
+
+async function getBookmarkIdByPost(user_id, post_id) {
+	const bookmark = await Bookmark.findOne({
 		user: user_id,
-	};
+		post: post_id,
+	});
 
-	const bookmark = new Bookmark(tempBookmark);
-
-	try {
-		await bookmark.save();
-		res.status(201).json({
-			saved: true,
-			message: 'Bookmark saved successfully!',
-		});
-	} catch (error) {
-		res.status(error.status || 500);
-
-		res.json({
-			succes: false,
-			error: error.message,
-		});
+	if (!bookmark) {
+		throw createError(404, 'Bookmark not found');
 	}
-};
 
-const getAllBookmarks = async (req, res) => {
-	const { user_id } = req.user;
+	return bookmark;
+}
 
-	try {
-		const bookmarks = await Bookmark.find({ user: user_id }).sort({
-			created_at: -1,
-		});
+async function deleteBookmark(user_id, post_id) {
+	const bookmark = await Bookmark.findOneAndDelete({
+		user: user_id,
+		post: post_id,
+	});
 
-		if (!bookmarks) {
-			throw createError(404, 'No bookmarks found');
-		}
-
-		res.status(200).json(bookmarks);
-	} catch (error) {
-		res.status(error.status || 500);
-
-		res.json({
-			succes: false,
-			error: error.message,
-		});
+	if (!bookmark) {
+		throw createError(404, 'Bookmark not found');
 	}
-};
 
-const getBookmarkIdByPost = async (req, res) => {
-	const { user_id } = req.user;
-	const { post_id } = req.params;
-
-	try {
-		const bookmark = await Bookmark.findOne({
-			user: user_id,
-			post: post_id,
-		});
-
-		if (!bookmark) {
-			throw createError(404, 'Bookmark not found');
-		}
-
-		res.status(200).json(bookmark);
-	} catch (error) {
-		res.status(error.status || 500);
-
-		res.json({
-			succes: false,
-			error: error.message,
-		});
-	}
-};
-
-const deleteBookmark = async (req, res) => {
-	const { user_id } = req.user;
-	const { post_id } = req.params;
-
-	try {
-		const bookmark = await Bookmark.findOneAndDelete({
-			user: user_id,
-			post: post_id,
-		});
-
-		if (!bookmark) {
-			throw createError(404, 'Bookmark not found');
-		}
-
-		res.status(200).json({
-			deleted: true,
-			message: 'Bookmark deleted successfully!',
-		});
-	} catch (error) {
-		res.status(error.status || 500);
-
-		res.json({
-			succes: false,
-			error: error.message,
-		});
-	}
-};
+	return bookmark;
+}
 
 module.exports = {
 	createBookmark,
